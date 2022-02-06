f3085f6892dadd8dcb76d4d900e681462a19f80d
Luigi Siano <lsiano@symplr.com>
2022-02-06 11:48:47 -0300

    Adding Interface and creating Dictionary class

---
+++ src/main/java/Dictionaries/EnglishDictionary.java
@@ -0,0 +1,11 @@
+package Dictionaries;
+
+import Interfaces.IDictionary;
+
+public class EnglishDictionary implements IDictionary {
+
+    @Override
+    public Boolean isEnglishWord(String word) {
+        return null;
+    }
+}
---
+++ src/main/java/Interfaces/IDictionary.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface IDictionary {
+    Boolean isEnglishWord(String word);
+}
